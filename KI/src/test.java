1a4368b4258f1fe01213710e7b4167a6ec21c06b
Marshmelooo97 <91906479+Marshmelooo97@users.noreply.github.com>
2023-04-24 19:10:03 +0200

    sdf

--- KI/src/test.java
+++ KI/src/test.java
@@ -1,16 +1,34 @@
 
 public class test {
      public static void main(String[] args) {
-          //printArray(f(new double[] { 0.0, 1.0 }));
-          //print2DArray(f(new double[][] { { -1.0 } }));
-          System.out.println(f(new double[] { 0.0, 1.0 ,9.,7.}));
+          // printArray(f(new double[] { 0.0, 1.0 }));
+          // print2DArray(f(new double[][] { { -1.0 } }));
+          // System.out.println(f(new double[] { 0.0, 1.0, 9., 7. }));
+          double[][] T1 = new double[][] { { 1., 0., 0. }, { 0., 0., 1. }, { 0., 1., 0. } };
+          System.out.println(
+                    classAccuracy(new double[][] { { 0.5, 0.4, 0.1 }, { 0.4, 0., 0.6 }, { 0., 0.9, 0.1 } }, T1));
+     }
+
+     public static double classAccuracy(double[][] Y, double[][] T) {
+          int i = 0;
+          double erg = 0;
+          int z = 0;
+          for (double[] ds : Y) {
+               z = (int) f(ds);
+               if (T[i][z] != 1) {
+                    erg += 1;
+               }
+               i += 1;
+          }
+          erg = erg / T.length;
+          return erg;
      }
 
      public static double f(double[] x) {
           double erg = 0;
           double index = 0;
           for (int i = 0; i < x.length; i++) {
-               if (x[i]> erg ) {
+               if (x[i] > erg) {
                     erg = x[i];
                     index = i;
                }
@@ -18,27 +36,26 @@ public class test {
           return index;
      }
 
-
-
-
      public static double[][] f(double[][] X) {
           double[][] erg = new double[X.length][2];
           double[] y = new double[] { 1.0, 0.0, 0.0 };
           for (int i = 0; i < X.length; i++) {
-               erg[i] = fVecVec(y); 
+               erg[i] = fVecVec(y);
           }
           return erg;
      }
-     /*public static double[] f(double[] x) {
-          if (x[0] < 0) {
-               double[] erg = new double[] { 1.0, 0.0, 0.0 };
-               return erg;
-          } else {
-               double[] erg = new double[] { 0.0, 1.0, 0.0 };
-               return erg;
-          }
-
-     }*/
+     /*
+      * public static double[] f(double[] x) {
+      * if (x[0] < 0) {
+      * double[] erg = new double[] { 1.0, 0.0, 0.0 };
+      * return erg;
+      * } else {
+      * double[] erg = new double[] { 0.0, 1.0, 0.0 };
+      * return erg;
+      * }
+      * 
+      * }
+      */
 
      public static double[] fVecVec(double[] x) {
           double[] ret = new double[] { 1., 0. };
